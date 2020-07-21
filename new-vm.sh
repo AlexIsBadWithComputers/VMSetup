82d4d6fe2b6fdbe1ff7aa4ee90258af8845723cb
AlexIsBadWithComputers <alex.tennant@cybera.ca>
2020-07-20 19:21:41 -0600

    hopefully final

--- new-vm.sh
+++ new-vm.sh
@@ -14,22 +14,22 @@ if [ ! -f /var/run/resume-after-boot ]; then
     # sudo apt-get --purge remove "libcudnn7*" -s -y
     # sudo apt-get --purge remove "*nvidia*" -s -y
 
-    # echo "First step instalation of packages...."
-    # # adding tensorflow/cuda libraries 
-    # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-    # sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-    # sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
-    # sudo apt-get update
-    # wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-    # sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb -s
-    # sudo apt-get update
-    # # python
-    # echo "Python installation...."
-    # sudo apt-get install python3 -s -y
-    # sudo apt-get install python3-pip -s -y
-    # # cuda
-    # echo "Cuda Drivers...." 
-    # sudo apt-get install --no-install-recommends nvidia-driver-450 -s -y 
+    echo "First step instalation of packages...."
+    # adding tensorflow/cuda libraries 
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+    sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+    sudo apt-get update
+    wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+    sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb 
+    sudo apt-get update
+    # python
+    echo "Python installation...."
+    sudo apt-get install python3  -y
+    sudo apt-get install python3-pip -y
+    # cuda
+    echo "Cuda Drivers...." 
+    sudo apt-get install --no-install-recommends nvidia-driver-450 -y 
 
     script="bash /home/ubuntu/VMSetup/new-vm.sh"
     echo "$script" >> ~/.bashrc 
@@ -51,7 +51,6 @@ else
     # get rid of the file so we don't consistently keep rebooting 
     sudo rm -f /var/run/resume-after-boot
     touch /home/ubuntu/made_it.txt
-    sudo echo -n >/home/ubuntu/made_it2.txt
     # test if nvidia-smi works
     if [ $? -eq 0 ]; then
         echo "Nvidia works!"
@@ -62,21 +61,21 @@ else
 
     # # Start installing CUDA and tensorflow
     # echo "Installing CUDA and CuDNN"
-    # sudo apt-get install --no-install-recommends cuda-10-1 -s
-    # sudo apt-get install --no-install-recommends libcudnn7=7.6.4.38-1+cuda10.1  -s
-    # sudo apt-get install --no-install-recommends libcudnn7-dev=7.6.4.38-1+cuda10.1 -s
-    # sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 -s 
-    # sudo apt-get install -y --no-install-recommendslibnvinfer-dev=6.0.1-1+cuda10.1 -s 
-    # sudo apt-get install -y --no-install-recommendslibnvinfer-plugin6=6.0.1-1+cuda10.1 -s 
+    sudo apt-get install --no-install-recommends cuda-10-1 
+    sudo apt-get install --no-install-recommends libcudnn7=7.6.4.38-1+cuda10.1  
+    sudo apt-get install --no-install-recommends libcudnn7-dev=7.6.4.38-1+cuda10.1 
+    sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 
+    sudo apt-get install -y --no-install-recommendslibnvinfer-dev=6.0.1-1+cuda10.1 
+    sudo apt-get install -y --no-install-recommendslibnvinfer-plugin6=6.0.1-1+cuda10.1 
 
-    # echo "Installing Python Packages"
-    # pip3 install numpy pandas jupyter scikit-learn networkx 
-    # echo "Installing tensorflow"
-    # pip3 install tensorflow-gpu
-    # echo "Installing pytorch"
-    # pip3 install torch==1.5.1+cu101 torchvision==0.6.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-    # echo "Installing keras"
-    # pip3 install keras
+    echo "Installing Python Packages"
+    pip3 install numpy pandas jupyter scikit-learn networkx 
+    echo "Installing tensorflow"
+    pip3 install tensorflow-gpu
+    echo "Installing pytorch"
+    pip3 install torch==1.5.1+cu101 torchvision==0.6.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+    echo "Installing keras"
+    pip3 install keras
 
 fi
 
